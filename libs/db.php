f0e1345d7c1174c98392de878913aa5f37cfd4ed
Enric <hairowski@gmail.com>
2017-03-09 17:43:53 +0100

    Update db.php

--- libs/db.php
+++ libs/db.php
@@ -6,7 +6,7 @@ include "libs/ez_sql_core.php";
 include "libs/ez_sql_mysql.php";
 
 function db_connect(){
-	$db = new ezSQL_mysql('enric','roqueta','intranet','localhost'); 
+	$db = new ezSQL_mysql('user','password','intranet','localhost'); 
 	$db->query("SET NAMES 'utf8'");
 	// For production servers
 	//$db->hide_errors();
